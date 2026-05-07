3a7d0b234ad95d3d6a3376934a94e77af10df0db
Jonathan Ong <73453714+ongaikngee@users.noreply.github.com>
2026-05-07 14:38:06 +0800

    added todo components

---
+++ src/TodoList.jsx
@@ -0,0 +1,12 @@
+export default function TodoList({completedTodos, incompletedTodos}) {
+    return (
+        <div>
+            <h1>My Todos</h1>
+            <p>New todo fom will go here....</p>
+
+            <h3>Completed:</h3>
+
+            <h3>Incomplete:</h3>
+        </div>
+    )
+}
