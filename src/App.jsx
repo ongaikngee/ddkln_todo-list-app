293ced2dc480f84820f49ab4f1f15e13b5f80ab2
Jonathan Ong <73453714+ongaikngee@users.noreply.github.com>
2026-05-08 11:00:43 +0800

    feat: add loadingSlice and thunks for loading state management, integrate axios for API calls

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { loadTodos } from "./thunks";
 import "./App.css";
 import TodoList from "./todolist";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadTodos());
+  }, []);
+
   return (
     <>
       <TodoList />
